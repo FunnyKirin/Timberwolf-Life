20c3f0b403fb635709de4ecbc99279819a6e124c
Jimmy Cheng <wenliang.cheng@stonybrook.edu>
2016-11-07 16:44:45 -0500

    change sign out icon

--- WarGrid/public/scripts/app.js
+++ WarGrid/public/scripts/app.js
@@ -1,4 +1,5 @@
 // Initialize Firebase
+
 var config = {
     apiKey: "AIzaSyCg8q3JPOVremSm5Exz74by1Rsv4ljk970",
     authDomain: "llylly-95353.firebaseapp.com",
@@ -7,3 +8,15 @@ var config = {
     messagingSenderId: "745498047349"
 };
 firebase.initializeApp(config);
+
+
+
+/*
+var config = {
+    apiKey: "AIzaSyBmJDK9mM-pzPKJWRvijmkgID0GceHCWMs",
+    authDomain: "dark-liver-life-build-2.firebaseapp.com",
+    databaseURL: "https://dark-liver-life-build-2.firebaseio.com",
+    storageBucket: "dark-liver-life-build-2.appspot.com",
+    messagingSenderId: "261875659167"
+  };
+*/
