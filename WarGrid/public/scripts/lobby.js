d151303fc0e34ba8e924bd82e515a324d378dfa2
steven <heh@127.0.0.1>
2016-11-12 16:10:35 -0500

    saodmvlsamflksadnfksadnvoasdf

--- WarGrid/public/scripts/lobby.js
+++ WarGrid/public/scripts/lobby.js
@@ -20,25 +20,31 @@ Lobby.prototype.lobbyHandler = function(player) {
 };
 
 Lobby.prototype.init = function() {
-    var count = 0;
 
     this.ref = firebase.database().ref();
     this.auth = firebase.auth();
 
-    this.ref.child('maps').on('child_added', function(snapshot) {
+    this.ref.child('maps').on('value', function(snapshot) {
+        var count = 0;
+        var innerHTML = "";
+
         snapshot.forEach(function(data) {
-            var innerHTML = "\<div class=\"w3-third w3-panel\"\>";
+            innerHTML += "\<div class=\"w3-third w3-panel\"\>";
             innerHTML += "\<div name=\"myCards\" class=\"w3-card-12\"\>\<img src=\"";
             innerHTML += "https://firebasestorage.googleapis.com/v0/b/wargrid-cbca4.appspot.com/o/images%2Fmap_t_1.PNG?alt=media&token=636a2622-cb06-473d-8144-3efa2a92a186\"";
             innerHTML += "; style=\"width:100%\" ; onclick=\"game_open()\"\>";
             innerHTML += "\<p class=\"w3-left\"\>RoomName1\<\/p\>\<p class=\"w3-right\"\>OwnerName1\<\/p\>\<\/div\>\<\/div\>";
-            $("#" + ROOM_GRID_ID).html(innerHTML);
+
+
+            count += 1;
         });
 
-        count += 1;
+        $("#" + ROOM_GRID_ID).html(innerHTML);
+
+        console.log("Number of maps: ", count);
     });
 
-    console.log("Number of maps: ", count);
+
 
     this.auth.onAuthStateChanged(this.lobbyHandler.bind(this));
 };
