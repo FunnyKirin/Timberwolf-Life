92455b21279a8803008bc46a024157ce0cd4ee8b
steven <heh@127.0.0.1>
2016-11-11 20:24:19 -0500

    sdf

--- WarGrid/public/scripts/load.js
+++ WarGrid/public/scripts/load.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // initialization of all models
     window.map = new Map();
     window.session = new Session();
     window.player = new Player();
--- WarGrid/public/scripts/player.js
+++ WarGrid/public/scripts/player.js
@@ -6,6 +6,8 @@ var PLAYER_ID_ID = "player-id";
 var PLAYER_PROFILE_ID = "player-profile-desktop";
 var PLAYER_PROFILE_ID_2 = "player-profile-mobile";
 
+var playerId;
+
 var Player = function() {
     console.log('[INFO] Loading Player Module...');
 
@@ -15,36 +17,68 @@ var Player = function() {
     this.buttonSigninFacebook = document.getElementById(FACEBOOK_SIGNIN_ID);
     this.buttonSignout = document.getElementById(SIGNOUT_ID);
     this.buttonProfile = document.getElementById(PLAYER_PROFILE_ID);
-/*
-    this.buttonSigninGoogle.addEventListener('click', this.googleSignIn.bind(this));
-    this.buttonSigninFacebook.addEventListener('click', this.facebookSignIn.bind(this));
-    this.buttonSignout.addEventListener('click', this.signOut.bind(this));
-*/
+
     this.init();
 };
 
 Player.prototype.playerHandler = function(player) {
     if (player) { // logged in
         var profilePicUrl = player.photoURL;
-        var playerId = player.displayName;
+        var displayName = player.displayName;
         var email = player.email;
 
+        // html element display
         $("." + GOOGLE_SIGNIN_ID).hide();
         $("." + FACEBOOK_SIGNIN_ID).hide();
         $("." + SIGNOUT_ID).show();
-        $("." + PLAYER_PROFILE_ID).html("<i class=\"material-icons\">person</i> " + playerId);
+        $("." + PLAYER_PROFILE_ID).html("<i class=\"material-icons\">person</i> " + displayName);
 
-        console.log("Hello, ", playerId);
-        console.log("You signed in with ", email);
+        // database management of players
+        this.ref.child('player').child(email).once('value', function(pid) {
+            if (pid) { // user already registered
+                this.ref.child('player').child(pid).once('value', function(player) {
+                    var p = player.val();
+                    p[online] = true;
+                    this.ref.child('player').child(pid).update(p);
+                });
+            } else { // prompt user to register
+                var id = prompt("First time? Get yourself a unique name");
+                this.ref.child('player').child(pid).once('value', function(attempt) {
+                    if (attempt) { // lol username taken
+                        alert('lol Player ID taken');
+                    } else { // cool, update database
+                        var p = {
+                            online: true,
+                            id: id,
+                            totalWins: 0,
+                            history: {}
+                        };
+                        this.ref.child('player').push(p);
+                        playerId = id;
+                    }
+                }, function(error) {
+                    console.log("[ERROR] Error signing in player: ", error);
+                });
+            }
+        }, function(error) {
+            console.log("[ERROR] Error signing in player: ", error);
+        });
+
+        //console.log("Hello, ", playerId);
+        //console.log("You signed in with ", email);
 
     } else { // logged out
-
         $("." + GOOGLE_SIGNIN_ID).show();
         $("." + FACEBOOK_SIGNIN_ID).show();
         $("." + SIGNOUT_ID).hide();
         $("." + PLAYER_PROFILE_ID).html("<i class=\"material-icons\">person</i> Login");
 
-        console.log("Bye");
+        // player offline
+        this.ref.child('player').child(playerId).once('value', function(player) {
+            var p = player.val();
+            p[online] = false;
+            this.ref.child('player').child(playerId).update(player);
+        });
     }
 };
 
@@ -76,6 +110,7 @@ Player.prototype.signOut = function() {
 
 Player.prototype.init = function() {
     // Shortcuts to Firebase SDK features.
+    this.ref = firebase.database().ref();
     this.auth = firebase.auth();
     // Initiates Firebase auth and listen to auth state changes.
     this.auth.onAuthStateChanged(this.playerHandler.bind(this));
