af78b22334b80309e19930063144cbdbcd8c29ad
steven <heh@127.0.0.1>
2016-11-12 22:25:29 -0500

    update

--- WarGrid/public/scripts/createPage.js
+++ WarGrid/public/scripts/createPage.js
@@ -13,7 +13,11 @@ function loadMaps() {
             innerHTML += "\<div name=\"myCards\" class=\"w3-card-12\"\>\<img src=\"";
             innerHTML += "https://firebasestorage.googleapis.com/v0/b/wargrid-cbca4.appspot.com/o/images%2Fmap_t_1.PNG?alt=media&token=636a2622-cb06-473d-8144-3efa2a92a186\"";
             innerHTML += "; style=\"width:100%\" ; onclick=\"game_open()\"\>";
+<<<<<<< HEAD
             innerHTML += "\<p class=\"w3-left\"\>"+ snapshot.val() + "\<\/p\>\<p class=\"w3-right\"\>OwnerName1\<\/p\>\<\/div\>\<\/div\>";
+=======
+            innerHTML += "\<p class=\"w3-left\"\>"+data.val().map + "\<\/p\>\<p class=\"w3-right\"\>"+ data.val().creator+"\<\/p\>\<\/div\>\<\/div\>";
+>>>>>>> b1893adda412de5887b5abd08d1de756df5f14bb
             count += 1;
         });
 
