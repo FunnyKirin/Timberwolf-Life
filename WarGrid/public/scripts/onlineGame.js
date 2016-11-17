05a8cedefd6caaa7e70c7856624456388f763a92
Zhengyang Liu <liuzhengyang183@gmail.com>
2016-11-17 18:21:26 -0500

    network

--- WarGrid/public/scripts/onlineGame.js
+++ WarGrid/public/scripts/onlineGame.js
@@ -163,7 +163,6 @@ function initFirebase() {
         if (snapshot.val().currentPlayer == playerIndex) {
             nextTurn();
         }
-        alert("you are " + playerIndex);
     });
     // Initiates Firebase auth and listen to auth state changes.
     //this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
@@ -426,6 +425,10 @@ function confirmMove() {
 function writeMap(grid) {
     room.child("grid").transaction(function (currentData) {
         return grid;
+    });
+    room.child("currentPlayer").transaction(function (currentData) {
+        currentData = currentData === 1 ? 2 : 1;
+        return currentData;
     });
 }
 //check if current player achieved victory.
