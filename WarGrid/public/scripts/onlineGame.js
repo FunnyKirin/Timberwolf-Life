88d56327d499c520d9596001ee58080a316d4a65
Zhengyang Liu <Zhengyang.Liu@stonybrook.edu>
2016-12-20 01:11:41 -0500

    change game rule

--- WarGrid/public/scripts/onlineGame.js
+++ WarGrid/public/scripts/onlineGame.js
@@ -154,16 +154,17 @@ function initFirebase() {
         anonymous = 1;
     }
     room = database.ref().child("lobby").child(url);
-    room.once('value', function(snapshot) {
+    room.once('value', function (snapshot) {
         currentPlayer = snapshot.val().currentPlayer;
         renderGrid = snapshot.val().grid;
         loadMapName = snapshot.val().map;
         //get player Index
         if (anonymous === 0) {
-            database.ref().child("playerUID").child(auth.currentUser.uid).once("value", function(abc) {
+            database.ref().child("playerUID").child(auth.currentUser.uid).once("value", function (abc) {
                 if (abc.val() == snapshot.val().owner) {
                     playerIndex = 1;
-                } else if (abc.val() == snapshot.val().challenger) {
+                }
+                else if (abc.val() == snapshot.val().challenger) {
                     playerIndex = 2;
                 }
                 if (snapshot.val().currentPlayer == playerIndex) {
@@ -174,8 +175,8 @@ function initFirebase() {
         //swal("you are player " + playerIndex);
     });
     var map = database.ref().child("maps");
-    map.once("value").then(function(snapshot) {
-        snapshot.forEach(function(data) {
+    map.once("value").then(function (snapshot) {
+        snapshot.forEach(function (data) {
             if (data.val().map == loadMapName) {
                 var x = data.val().x;
                 if (canvasWidth % x !== 0) {
@@ -197,7 +198,7 @@ function initFirebase() {
     });
     // Initiates Firebase auth and listen to auth state changes.
     //this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
-    room.on('value', function(snapshot) {
+    room.on('value', function (snapshot) {
         //swal(snapshot.val().currentPlayer + " + " + currentPlayer);
         //switch player
         if (snapshot.val().currentPlayer != currentPlayer) {
@@ -246,7 +247,7 @@ function initEventHandlers() {
     canvas.onclick = respondToMouseClick;
     $("#confirmButton").click(confirmMove);
     //click ghostButton will enable/disable ghostcells
-    $("#ghostButton").click(function() {
+    $("#ghostButton").click(function () {
         ghostTrigger = ghostTrigger === 1 ? 2 : 1;
         //re-render game after clicking.
         renderGame();
@@ -254,7 +255,7 @@ function initEventHandlers() {
         renderGhost();
         renderGridLines();
     });
-    $("#resetButton").click(function() {
+    $("#resetButton").click(function () {
         cellNumber = getCellNumber(territory);
         ghostGrid = [];
         //re-render game after clicking.
@@ -263,22 +264,21 @@ function initEventHandlers() {
         renderGhost();
         renderGridLines();
     });
-    $("#inviteFriendButton").click(function() {
+    $("#inviteFriendButton").click(function () {
         var url = window.location.href;
         if (url.charAt(url.length - 4) === "%") {
             var url = url.substring(0, url.length - 1) + "2";
-        } else {
+        }
+        else {
             url = url + "%202";
         }
         $("#invitedURL").val(url);
         var clipboard = new Clipboard("#copy-button");
     });
-    $("#copyurl").click(function() {
-
-    });
+    $("#copyurl").click(function () {});
     // confirms leaving before actually leaving otherwise users may leave without actually want to leave
     // tongue twister level 1
-    window.onbeforeunload = function() {
+    window.onbeforeunload = function () {
         return 'If you leave, you lose';
     };
     // remove game room properly
@@ -305,7 +305,7 @@ function respondToMouseClick(event) {
         var ghostCell = getGridCell(ghostGrid, clickRow, clickCol);
         //check if there is already a cell in ghost grid,
         // if not:
-        if (cell != LIVE_CELL + playerIndex * 10 && cell != LIVE_CELL + (3 - playerIndex) * 10) {
+        if (cell != LIVE_CELL + playerIndex * 10) {
             if (ghostCell != LIVE_CELL + playerIndex * 10) {
                 //check if the player can place a cell at that position.
                 if (cellNumber > 0 && cell != VOID_CELL) {
@@ -333,8 +333,16 @@ function respondToMouseClick(event) {
                     }
                     //it is!
                     if (boolean == 1) {
-                        setGridCell(ghostGrid, clickRow, clickCol, LIVE_CELL + playerIndex * 10);
-                        cellNumber--;
+                        if (cell == LIVE_CELL + (3 - playerIndex) * 10) {
+                            if (cellNumber >= 2) {
+                                setGridCell(ghostGrid, clickRow, clickCol, LIVE_CELL + playerIndex * 10);
+                                cellNumber -= 2;
+                            }
+                        }
+                        else {
+                            setGridCell(ghostGrid, clickRow, clickCol, LIVE_CELL + playerIndex * 10);
+                            cellNumber--;
+                        }
                     }
                 }
             }
@@ -342,6 +350,9 @@ function respondToMouseClick(event) {
             else {
                 setGridCell(ghostGrid, clickRow, clickCol, 0);
                 cellNumber++;
+                if (cell == LIVE_CELL + (3 - playerIndex) * 10) {
+                    cellNumber++;
+                }
                 //if boolean2 ==1, we cant withdraw that placement.
                 var boolean2 = 0;
                 for (var i = 0; i <= gridHeight; i++) {
@@ -358,6 +369,9 @@ function respondToMouseClick(event) {
                 if (boolean2 == 1) {
                     setGridCell(ghostGrid, clickRow, clickCol, LIVE_CELL + currentPlayer * 10);
                     cellNumber--;
+                    if (cell == LIVE_CELL + (3 - playerIndex) * 10) {
+                        cellNumber--;
+                    }
                 }
             }
             //reset game UI
@@ -432,32 +446,15 @@ function renderGhostCells() {
                 var _y = i * cellLength;
                 if (_leftNumber == playerIndex) {
                     if (_rightNumber == 1) {
-                        canvas2D.fillStyle = GHOST_COLOR;
-                        canvas2D.fillRect(_x, _y, cellLength, cellLength);
+                        canvas2D.beginPath();
+                        canvas2D.lineWidth = "4"; //ghostWidth
+                        canvas2D.strokeStyle = GHOST_COLOR;
+                        canvas2D.rect(_x + 2, _y + 2, cellLength - 4, cellLength - 4);
+                        canvas2D.stroke();
                     }
                 }
             }
-            /*
-             //if the cell is a player's living/ dead cell
-             if (leftNumber > 0) {
-             //it is a deadcell
-             if (rightNumber === 0) {
-             canvas2D.fillStyle = DEAD_COLOR[leftNumber];
-             canvas2D.fillRect(x, y, cellLength, cellLength);
-             }
-             //it is a living cell
-             else {
-             canvas2D.fillStyle = LIVE_COLOR[leftNumber];
-             canvas2D.fillRect(x, y, cellLength, cellLength);
-             }
-             }
-             //it is a void cell
-
-             if (rightNumber == 3) {
-             canvas2D.fillStyle = VOID_COLOR;
-             canvas2D.fillRect(x, y, cellLength, cellLength);
-             }
-             */
+            
         }
     }
 }
@@ -514,10 +511,10 @@ function confirmMove() {
 }
 //send map info to database after pressing confirm
 function writeMap(grid) {
-    room.child("grid").transaction(function(currentData) {
+    room.child("grid").transaction(function (currentData) {
         return grid;
     });
-    room.child("currentPlayer").transaction(function(currentData) {
+    room.child("currentPlayer").transaction(function (currentData) {
         currentData = currentData === 1 ? 2 : 1;
         return currentData;
     });
@@ -565,7 +562,8 @@ function getCellNumber(territory) {
         if (territory > 0) {
             number++;
             size += 2;
-        } else {
+        }
+        else {
             break;
         }
     }
@@ -682,7 +680,8 @@ function updateGame(updateGrid, renderGrid) {
                 if (numLivingNeighbors === 3) {
                     //become a live cell
                     renderGrid[index] = LIVE_CELL + 10 * playerIndex;
-                } else if (testCell == DEAD_CELL) {
+                }
+                else if (testCell == DEAD_CELL) {
                     {
                         //still a dead cell
                         renderGrid[index] = DEAD_CELL;
@@ -722,7 +721,8 @@ function renderCells() {
                 if (rightNumber === 0) {
                     canvas2D.fillStyle = DEAD_COLOR[leftNumber];
                     canvas2D.fillRect(x, y, cellLength, cellLength);
-                } else {
+                }
+                else {
                     canvas2D.fillStyle = LIVE_COLOR[leftNumber];
                     canvas2D.fillRect(x, y, cellLength, cellLength);
                 }
@@ -736,6 +736,8 @@ function renderCells() {
 }
 
 function renderGridLines() {
+    
+    canvas2D.lineWidth = "1";
     // SET THE PROPER COLOR
     canvas2D.strokeStyle = GRID_LINES_COLOR;
     // VERTICAL LINES
@@ -873,13 +875,14 @@ function isValidCell(row, col) {
 function getRelativeCoords(event) {
     if (event.offsetX !== undefined && event.offsetY !== undefined) {
         return {
-            x: event.offsetX,
-            y: event.offsetY
+            x: event.offsetX
+            , y: event.offsetY
         };
-    } else {
+    }
+    else {
         return {
-            x: event.layerX,
-            y: event.layerY
+            x: event.layerX
+            , y: event.layerY
         };
     }
 }
@@ -887,29 +890,28 @@ function getRelativeCoords(event) {
 function checkSetup() {
     if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
         swal('You have not configured and imported the Firebase SDK. ' + 'Make sure you go through the codelab setup instructions.');
-    } else if (config.storageBucket === '') {
+    }
+    else if (config.storageBucket === '') {
         swal('Your Firebase Storage bucket has not been enabled. Sorry about that. This is ' + 'actually a Firebase bug that occurs rarely. ' + 'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' + 'and make sure the storageBucket attribute is not empty. ' + 'You may also need to visit the Storage tab and paste the name of your bucket which is ' + 'displayed there.');
     }
 }
-
 // p1, p2 are strings
 // p1 wins, p2 loses
-var gameOver = function(p1, p2) {
+var gameOver = function (p1, p2) {
     // reference
     var playerRef = firebase.database().ref('players');
     // p1 wins
-    playerRef.child(p1).child('totalWins').transaction(function(wins) {
+    playerRef.child(p1).child('totalWins').transaction(function (wins) {
         return wins + 1;
     });
     // p2 wins
-    playerRef.child(p2).child('totalLosses').transaction(function(losses) {
+    playerRef.child(p2).child('totalLosses').transaction(function (losses) {
         return losses + 1;
     });
 };
-
 // handles the operation of leaving a game room
 // this is called when a user closes a tab
-var leaveRoom = function() {
+var leaveRoom = function () {
     var raw_key = window.location.search.substring(1); // the raw room key, we need to mod it
     var room_key = playerId ? raw_key : raw_key.slice(0, -4); // anonymous links are different!
     var mapRef = firebase.database().ref('maps');
@@ -917,34 +919,31 @@ var leaveRoom = function() {
     var roomRef = lobbyRef.child(room_key); // game session
     var challenger = roomRef.child('challenger'); // the challenger
     var owner = roomRef.child('owner'); // the owner
-
     if (playerId) { // if the player signed in
         // if the owner quits, the room disappears
-        owner.once('value', function(ownerSnap) {
+        owner.once('value', function (ownerSnap) {
             if (ownerSnap.val() == playerId) {
                 roomRef.remove();
             }
         });
-    } else { // guest player
+    }
+    else { // guest player
         // if it's the owner
         if (raw_key.slice(-1) == '1') {
             roomRef.remove();
         }
     }
-
-    roomRef.once('value', function(roomSnap) {
+    roomRef.once('value', function (roomSnap) {
         if (roomSnap.val()) { // check if the room exists first
-            challenger.once('value', function(challengerSnap) {
+            challenger.once('value', function (challengerSnap) {
                 // if an observer leaves nothing happens
-
                 if (challengerSnap.val() == playerId) {
                     // the challenger is gone whatsoever
-                    challenger.transaction(function(e) {
+                    challenger.transaction(function (e) {
                         return '';
                     });
-
-                    mapRef.child(roomSnap.val().map).once('value', function(mapSnap) {
-                        roomSnap.child('grid').transaction(function(grid) {
+                    mapRef.child(roomSnap.val().map).once('value', function (mapSnap) {
+                        roomSnap.child('grid').transaction(function (grid) {
                             return mapSnap.val().data;
                         });
                     });
